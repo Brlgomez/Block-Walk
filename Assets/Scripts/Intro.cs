53df0463ce90c87303925e68ea7749a895e4966f
Brandon Gomez <brlgomez@ucsc.edu>
2017-02-13 19:26:06 -0800

    Installed Google Play Services

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Intro : MonoBehaviour {
 
@@ -7,13 +8,32 @@ public class Intro : MonoBehaviour {
 
 	float timer = 1f;
 	Color32 nextColor = MenuColors.menuColor;
+	GameObject text;
+
+	void Start () {
+		text = GameObject.Find("Title Text");
+	}
 
 	void Update () {
 		timer += Time.deltaTime;
+		Camera.main.backgroundColor = Color32.Lerp(Camera.main.backgroundColor, nextColor, Time.deltaTime);	
 		if (timer > timerLimit) {
 			timer = 0;
 			nextColor = new Color(Random.Range (0.5f, 1.0f), Random.Range (0.5f, 1.0f), Random.Range (0.5f, 1.0f), 1);
 		}
-		Camera.main.backgroundColor = Color32.Lerp(Camera.main.backgroundColor, nextColor, Time.deltaTime);	
+		text.transform.localPosition = new Vector3(
+			text.transform.localPosition.x, 
+			text.transform.localPosition.y + pingPong(Time.time * 1, -2, 2), 
+			text.transform.localPosition.z
+		);
+			text.transform.rotation = Quaternion.Euler(
+			0, 
+			0, 
+			text.transform.rotation.eulerAngles.z + pingPong(Time.time * 2, -1.5f, 1.5f)
+		);
+	}
+
+	float pingPong(float aValue, float aMin, float aMax) {
+		return Mathf.PingPong(aValue, aMax-aMin) + aMin;
 	}
 }
