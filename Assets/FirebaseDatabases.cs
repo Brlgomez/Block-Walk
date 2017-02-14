d25ee0b6f5c5158f33de8a6b9400ff69a04623f6
Brandon Gomez <brlgomez@ucsc.edu>
2017-02-14 11:25:09 -0800

    Installed firebase

---
+++ Assets/FirebaseDatabases.cs
@@ -0,0 +1,52 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class FirebaseDatabases : MonoBehaviour {
+
+	DependencyStatus dependencyStatus = DependencyStatus.UnavailableOther;
+
+	void Start() {
+		dependencyStatus = FirebaseApp.CheckDependencies();
+		if (dependencyStatus != DependencyStatus.Available) {
+			FirebaseApp.FixDependenciesAsync().ContinueWith(task => {
+				dependencyStatus = FirebaseApp.CheckDependencies();
+				if (dependencyStatus == DependencyStatus.Available) {
+					InitializeFirebase();
+				} else {
+					Debug.LogError(
+						"Could not resolve all Firebase dependencies: " + dependencyStatus);
+				}
+			});
+		} else {
+			InitializeFirebase();
+		}
+	}
+
+	void InitializeFirebase() {
+		FirebaseApp app = FirebaseApp.DefaultInstance;
+		app.SetEditorDatabaseUrl("https://vox-voyager-87607159.firebaseio.com/");
+
+
+		FirebaseDatabase.DefaultInstance
+			.GetReference("Levels").OrderByChild("Date").LimitToLast(10).ValueChanged += (object sender2, ValueChangedEventArgs e2) => {
+			if (e2.DatabaseError != null) {
+				Debug.LogError(e2.DatabaseError.Message);
+				return;
+			}
+			if (e2.Snapshot != null && e2.Snapshot.ChildrenCount > 0) {
+				foreach (var childSnapshot in e2.Snapshot.Children) {
+					Debug.Log(childSnapshot.Child("Username").Value.ToString());
+					Debug.Log(childSnapshot.Child("Downloads").Value.ToString());
+					Debug.Log(childSnapshot.Child("Date").Value.ToString());
+					Debug.Log(childSnapshot.Child("Data").Value.ToString());
+				}
+			}
+		};
+	}
+}
---
+++ Assets/Scripts/GooglePlay.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public class GooglePlay : MonoBehaviour {
+
+	void Start () {
+		//if (PlayerPrefs.GetInt ("Online", 0) == 0) {
+			PlayGamesPlatform.DebugLogEnabled = false;
+			PlayGamesPlatform.Activate ();
+			logIn ();
+		//}
+	}
+
+	public void logIn() {
+		if (!Social.localUser.authenticated) {
+			Social.localUser.Authenticate ((bool success) => {
+				if (success) {
+					PlayerPrefs.SetInt ("Online", 0);
+					PlayerPrefs.Save ();
+				} else {
+					PlayerPrefs.SetInt ("Online", 1);
+					PlayerPrefs.Save ();
+				}
+			});
+		}
+	}
+
+	public void activateAchievements() {
+		Social.localUser.Authenticate((bool success) => {
+			if (success) {
+				PlayerPrefs.SetInt ("Online", 0);
+				PlayerPrefs.Save ();
+				Social.ShowAchievementsUI();
+			} else {
+				PlayerPrefs.SetInt ("Online", 1);
+				PlayerPrefs.Save ();
+			}
+		});
+	}
+}
